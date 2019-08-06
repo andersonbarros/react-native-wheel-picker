6fc21971accca82187d64d31bd1b51007dc123b7
Anderson Barros <anderson@iuqo.com>
2019-08-05 22:46:11 -0300

    remove linear gradient

--- android/src/main/java/com/zyu/ReactWheelCurvedPicker.java
+++ android/src/main/java/com/zyu/ReactWheelCurvedPicker.java
@@ -56,10 +56,6 @@ public class ReactWheelCurvedPicker extends WheelCurvedPicker {
 
         Paint paint = new Paint();
         paint.setColor(Color.WHITE);
-        int colorFrom = 0x00FFFFFF;//Color.BLACK;
-        int colorTo = Color.WHITE;
-        LinearGradient linearGradientShader = new LinearGradient(rectCurItem.left, rectCurItem.top, rectCurItem.right/2, rectCurItem.top, colorFrom, colorTo, Shader.TileMode.MIRROR);
-        paint.setShader(linearGradientShader);
         canvas.drawLine(rectCurItem.left, rectCurItem.top, rectCurItem.right, rectCurItem.top, paint);
         canvas.drawLine(rectCurItem.left, rectCurItem.bottom, rectCurItem.right, rectCurItem.bottom, paint);
     }
